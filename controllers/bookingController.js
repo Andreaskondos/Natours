f820f261c0847615cbfab5723dc9e78314271d37
Andreas Kondos <andreaskonkon@gmail.com>
2023-04-29 00:31:14 +0300

    Fixed bugg from stripe

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -60,7 +60,7 @@ exports.checkoutSession = catchAsync(async (req, res, next) => {
 const addBooking = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].price_data.unit_amount / 100;
+  const price = session.amount_total / 100;
 
   await Booking.create({ tour, user, price });
 };
--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@
 
 ## Finally done while working as first engineer and studing backend!
 
-### Learned from it: Node.js, mongoDB (mongoose), postman and pug
+### Learned from it: Node.js, express, mongoDB, postman and pug
