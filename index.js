c003f1c8a3bb6b8952366282183286e6e9138def
Andreas Kondos <andreaskonkon@gmail.com>
2024-04-25 17:33:50 +0300

    no3 try for upload on vercel

--- index.js
+++ index.js
@@ -1,148 +1,31 @@
-const path = require("path");
-const express = require("express");
-const morgan = require("morgan");
-const rateLimit = require("express-rate-limit");
-const helmet = require("helmet");
-const mongoSanitize = require("express-mongo-sanitize");
-const xss = require("xss-clean");
-const hpp = require("hpp");
-const cookieParser = require("cookie-parser");
-const compression = require("compression");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-const AppError = require("./utils/appError");
-const globalErrorHandler = require("./controllers/errorController");
-const tourRouter = require("./routes/tourRoutes");
-const userRouter = require("./routes/userRoutes");
-const reviewRouter = require("./routes/reviewRoutes");
-const bookingRouter = require("./routes/bookingRoutes");
-const bookingController = require("./controllers/bookingController");
-const viewRouter = require("./routes/viewRoutes");
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 
-const app = express();
+dotenv.config({ path: "./config.env" });
+const app = require("./app");
 
-// app.enable("trust proxy");
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
 
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
-
-// 1) GLOBAL MIDDLEWARES
-// Serving static files
-app.use(express.static(path.join(__dirname, "public")));
-
-// Set security HTTP headers
-app.use(helmet());
-
-//HELMET CONFIGURATION
-const scriptSrcUrls = [
-  "https://unpkg.com/",
-  "https://tile.openstreetmap.org",
-  "https://js.stripe.com",
-  // "https://cdnjs.cloudflare.com",
-];
-const styleSrcUrls = [
-  "https://unpkg.com/",
-  "https://tile.openstreetmap.org",
-  "https://fonts.googleapis.com/",
-  // "https://js.stripe.com",
-];
-const connectSrcUrls = [
-  "https://unpkg.com",
-  "https://tile.openstreetmap.org",
-  "ws://127.0.0.1:1234/",
-  "*.stripe.com",
-  "*.stripe.network",
-];
-const fontSrcUrls = ["fonts.googleapis.com", "fonts.gstatic.com"];
-
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: [],
-      connectSrc: ["'self'", ...connectSrcUrls],
-      scriptSrc: ["'self'", ...scriptSrcUrls],
-      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-      workerSrc: ["'self'", "blob:"],
-      objectSrc: [],
-      imgSrc: ["'self'", "blob:", "data:", "https:"],
-      fontSrc: ["'self'", ...fontSrcUrls],
-      frameSrc: ["*.stripe.com", "*.stripe.network"],
-    },
-  })
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
 );
-//////////////////////////////
-// Development logging
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-}
+mongoose.set("strictQuery", false);
+mongoose.connect(DB).then(() => console.log("DB connection successful"));
 
-// Limit requests from same API
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: "Too many requests from this IP, please try again in an hour!",
+process.on("unhandledRejection", (err) => {
+  console.log("UNCAUGHT REJECTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
-app.use("/api", limiter);
-
-//Stripe needs raw body and not in JSON form so we add here the webhook requests
-app.post(
-  "/webhook-checkout",
-  express.raw({ type: "application/json" }),
-  bookingController.webhookCheckout
-);
-
-// Body parser, reading / passing data from body into req.body
-app.use(express.json({ limit: "10kb" }));
-app.use(express.urlencoded({ extended: true, limit: "10kb" }));
-app.use(cookieParser());
-
-// Data sanitization against NoSQL query injection
-app.use(mongoSanitize());
-
-// Data sanitization against XSS
-app.use(xss());
-
-// Prevent parameter pollution
-app.use(
-  hpp({
-    whitelist: [
-      "duration",
-      "ratingsQuantity",
-      "ratingsAverage",
-      "maxGroupSize",
-      "difficulty",
-      "price",
-    ],
-  })
-);
-
-// Test middleware
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  //   console.log(req.cookies);
-  next();
-});
-
-app.use((req, res, next) => {
-  const { alert } = req.query;
-  if (alert === "bookingOk")
-    res.locals.alert =
-      "Your booking has been completed successfully. Please check your email for confirmation. If your booking doesn't show up here, check again later! ";
-  next();
-});
-
-app.use(compression());
-
-// 3) ROUTES
-app.use("/", viewRouter);
-app.use("/api/v1/tours", tourRouter);
-app.use("/api/v1/users", userRouter);
-app.use("/api/v1/reviews", reviewRouter);
-app.use("/api/v1/bookings", bookingRouter);
-
-app.all("*", (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
-});
-
-app.use(globalErrorHandler);
-
-module.exports = app;
