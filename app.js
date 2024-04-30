28fca622753b28abf00a7a2769de66f62a606361
Andreas Kondos <andreaskonkon@gmail.com>
2024-04-30 11:27:31 +0300

    try allow iframe on portfolio website no9

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
-// const rateLimit = require("express-rate-limit");
+const rateLimit = require("express-rate-limit");
 // const helmet = require("helmet");
-const mongoSanitize = require("express-mongo-sanitize");
+// const mongoSanitize = require("express-mongo-sanitize");
 // const xss = require("xss-clean");
 // const hpp = require("hpp");
 const cookieParser = require("cookie-parser");
