e0b2985a945aade1739850ec59ff3ce25a126cb1
Andreas Kondos <andreaskonkon@gmail.com>
2024-04-30 11:13:03 +0300

    try allow iframe on portfolio website no5

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 // Set security HTTP headers
-app.use(helmet());
+// app.use(helmet());
 
 //HELMET CONFIGURATION
 const scriptSrcUrls = [
